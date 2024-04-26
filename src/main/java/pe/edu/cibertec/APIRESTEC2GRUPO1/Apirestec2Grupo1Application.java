73f678cd27721d64abbdf24682d52b0e84358bf4
AdrianLlontop <rizzaga1234@gmail.com>
2024-04-26 10:13:18 -0500

    estructura del proyecto

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2GRUPO1/Apirestec2Grupo1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2GRUPO1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestec2Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestec2Grupo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/APIRESTEC2GRUPO1/Apirestec2Grupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2GRUPO1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirestec2Grupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
